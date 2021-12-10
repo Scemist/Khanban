832997302e87f7dc62ecd7aff0ff6c2eecf1e2fb
Lucas Scemist <scemist.lucas@gmail.com>
2021-12-10 15:18:10 -0300

    Estilização do template index
    Seguindo o estilo de página única de dashboard

--- resources/views/templates/index.blade.php
+++ resources/views/templates/index.blade.php
@@ -11,8 +11,47 @@
 		<title>Khanban</title>
 	</head>
 	<body>
-		<nav></nav>
+		<nav>
+			<a href="#">Início</a>
+			<a href="#">Projetos</a>
+			<a href="#">Board</a>
+			<a href="#">Configurações</a>
+		</nav>
 
-		@yield('conteudo')
+		<aside>
+			<div class="group">
+				<button>Projetos</button>
+				<ul>
+					<li>
+						<button>Criar Projeto</button>
+					</li>
+					<li>
+						<button>Gerenciar</button>
+					</li>
+				</ul>
+			</div>
+			
+			<div class="group">
+				<button>Usuário</button>
+				<ul>
+					<li>
+						<button>Novo Usuário</button>
+					</li>
+					<li>
+						<button>Ver Usuários</button>
+					</li>
+				</ul>
+			</div>
+		</aside>
+
+		<main>
+			@yield('conteudo')
+		</main>
+
+		<footer>
+			<p>
+				Todos os direitos reservados: <a href="https://github.com/Scemist">github.com/Scemist</a>© - 2021
+			</p>
+		</footer>		
 	</body>
 </html>
