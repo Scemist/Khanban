3d99cd4a68d89f2a47a12dcf8a7d12005e1fb19f
Lucas Scemist <scemist.lucas@gmail.com>
2022-02-07 11:38:14 -0300

    Encapsulamento dos métodos de draggable do board em uma class

--- public/js/board.js
+++ public/js/board.js
@@ -3,46 +3,82 @@ var __webpack_exports__ = {};
 /*!*******************************!*\
   !*** ./resources/js/board.js ***!
   \*******************************/
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
 var tarefas = document.querySelectorAll('.tarefa');
 var colunasBody = document.querySelectorAll('.coluna-body');
+
+var Kanboard = /*#__PURE__*/function () {
+  function Kanboard() {
+    _classCallCheck(this, Kanboard);
+  }
+
+  _createClass(Kanboard, null, [{
+    key: "dragstart",
+    value: function dragstart() {
+      colunasBody.forEach(function (colunaBody) {
+        return colunaBody.classList.add('highlight');
+      });
+      this.classList.add('is-dragging');
+    }
+  }, {
+    key: "dragend",
+    value: function dragend() {
+      colunasBody.forEach(function (colunaBody) {
+        return colunaBody.classList.remove('highlight');
+      });
+      this.classList.remove('is-dragging');
+      Kanboard.reorderlist();
+    }
+  }, {
+    key: "dragover",
+    value: function dragover() {
+      this.classList.add('over');
+      var tarefaSendoArrastada = document.querySelector('.is-dragging');
+      this.prepend(tarefaSendoArrastada);
+    }
+  }, {
+    key: "dragleave",
+    value: function dragleave() {
+      this.classList.remove('over');
+    }
+  }, {
+    key: "drop",
+    value: function drop() {
+      this.classList.remove('over');
+    }
+  }, {
+    key: "reorderlist",
+    value: function reorderlist() {
+      colunasBody.forEach(function (coluna) {
+        var count = 1;
+        var tarefas = Array.from(coluna.children);
+        tarefas.forEach(function (tarefa) {
+          tarefa.setAttribute('data-position', count);
+          count++;
+        });
+      });
+    }
+  }]);
+
+  return Kanboard;
+}();
+
 tarefas.forEach(function (tarefa) {
-  tarefa.addEventListener('dragstart', dragstart);
-  tarefa.addEventListener('dragend', dragend);
+  tarefa.addEventListener('dragstart', Kanboard.dragstart); // Acende a luz das colunas
+
+  tarefa.addEventListener('dragend', Kanboard.dragend); // Apaga a luz das colunas
 });
-
-function dragstart() {
-  colunasBody.forEach(function (colunaBody) {
-    return colunaBody.classList.add('highlight');
-  });
-  this.classList.add('is-dragging');
-}
-
-function dragend() {
-  colunasBody.forEach(function (colunaBody) {
-    return colunaBody.classList.remove('highlight');
-  });
-  this.classList.remove('is-dragging');
-  window.alert('Okay');
-}
-
 colunasBody.forEach(function (colunaBody) {
-  colunaBody.addEventListener('dragover', dragover);
-  colunaBody.addEventListener('dragleave', dragleave);
-  colunaBody.addEventListener('drop', drop);
+  colunaBody.addEventListener('dragover', Kanboard.dragover); // Realça a coluna atual
+
+  colunaBody.addEventListener('dragleave', Kanboard.dragleave); // Tira o realce da coluna atual
+
+  colunaBody.addEventListener('drop', Kanboard.drop); // Tira o realce da coluna atual
 });
-
-function dragover() {
-  this.classList.add('over');
-  var tarefaSendoArrastada = document.querySelector('.is-dragging');
-  this.appendChild(tarefaSendoArrastada);
-}
-
-function dragleave() {
-  this.classList.remove('over');
-}
-
-function drop() {
-  this.classList.remove('over');
-}
 /******/ })()
 ;
--- resources/js/board.js
+++ resources/js/board.js
@@ -1,38 +1,46 @@
 const tarefas = document.querySelectorAll('.tarefa')
 const colunasBody = document.querySelectorAll('.coluna-body')
 
+class Kanboard {
+	static dragstart() {
+		colunasBody.forEach(colunaBody => colunaBody.classList.add('highlight'))
+		this.classList.add('is-dragging')
+	}
+	static dragend() {
+		colunasBody.forEach(colunaBody => colunaBody.classList.remove('highlight'))
+		this.classList.remove('is-dragging')
+		Kanboard.reorderlist()
+	}
+	static dragover() {
+		this.classList.add('over')
+		const tarefaSendoArrastada = document.querySelector('.is-dragging')
+		this.prepend(tarefaSendoArrastada)
+	}
+	static dragleave() {
+		this.classList.remove('over')
+	}
+	static drop() {
+		this.classList.remove('over')
+	}
+	static reorderlist() {
+		colunasBody.forEach(coluna => {
+			let count = 1
+			let tarefas = Array.from(coluna.children)
+			tarefas.forEach(tarefa => {
+				tarefa.setAttribute('data-position' , count)
+				count++
+			})
+		})
+	}
+}
+
 tarefas.forEach(tarefa => {
-	tarefa.addEventListener('dragstart', dragstart)
-	tarefa.addEventListener('dragend', dragend)
+	tarefa.addEventListener('dragstart', Kanboard.dragstart) // Acende a luz das colunas
+	tarefa.addEventListener('dragend', Kanboard.dragend) // Apaga a luz das colunas
 })
-
-function dragstart() {
-	colunasBody.forEach(colunaBody => colunaBody.classList.add('highlight'))
-	this.classList.add('is-dragging')
-}
-
-function dragend() {
-	colunasBody.forEach(colunaBody => colunaBody.classList.remove('highlight'))
-	this.classList.remove('is-dragging')
-	window.alert('Okay')
-}
 
 colunasBody.forEach(colunaBody => {
-	colunaBody.addEventListener('dragover', dragover)
-	colunaBody.addEventListener('dragleave', dragleave)
-	colunaBody.addEventListener('drop', drop)
+	colunaBody.addEventListener('dragover', Kanboard.dragover) // Realça a coluna atual
+	colunaBody.addEventListener('dragleave', Kanboard.dragleave) // Tira o realce da coluna atual
+	colunaBody.addEventListener('drop', Kanboard.drop) // Tira o realce da coluna atual
 })
-
-function dragover() {
-	this.classList.add('over')
-	const tarefaSendoArrastada = document.querySelector('.is-dragging')
-	this.appendChild(tarefaSendoArrastada)
-}
-
-function dragleave() {
-	this.classList.remove('over')
-}
-
-function drop() {
-	this.classList.remove('over')
-}
