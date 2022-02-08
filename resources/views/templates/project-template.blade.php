e8683b1a6ab34deb0717e46b53e29feb82ec5b49
Lucas Scemist <scemist.lucas@gmail.com>
2022-02-08 16:03:57 -0300

    Nova aside do template projeto e estilo melhorado

--- resources/views/templates/project-template.blade.php
+++ resources/views/templates/project-template.blade.php
@@ -40,26 +40,42 @@
 				KhanBan
 			</div>
 
-			<div class="group">
-				<button>
-					<svg width="16" height="16" fill="currentColor" class="bi bi-easel3-fill" viewBox="0 0 16 16"><path d="M8.5 12v1.134a1 1 0 1 1-1 0V12h-5A1.5 1.5 0 0 1 1 10.5V3h14v7.5a1.5 1.5 0 0 1-1.5 1.5h-5Zm7-10a.5.5 0 0 0 0-1H.5a.5.5 0 0 0 0 1h15Z"/></svg>
-					Grupo
-				</button>
+			<div class="aside-group">
+				<label>Grupo</label>
 				<ul>	
-					<li><button id="criar-projeto">Adicionar Pessoa</button></li>
-					<li><a href="#"><button>Gerenciar</button></a></li>
+					<li>
+						<button id="criar-projeto">
+							<svg width="18" height="18" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16"><path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/></svg>
+							Adicionar Pessoa
+						</button>
+					</li>
+					<li>
+						<button>
+							<svg width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16"><path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/><path fill-rule="evenodd" d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"/><path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/></svg>
+							Participantes
+						</button>
+					</li>
 				</ul>
+				<hr>
 			</div>
-			
-			<div class="group">
-				<button>
-					<svg width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16"><path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/></svg>
-					Filtros
-				</button>
-				<ul>
-					<li><button>Vencendo Hoje</button></li>
-					<li><button>Para Mim</button></li>
+
+			<div class="aside-group">
+				<label>Filtros</label>
+				<ul>	
+					<li>
+						<button id="criar-projeto">
+							<svg width="16" height="16" fill="currentColor" class="bi bi-card-checklist" viewBox="0 0 16 16"><path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/><path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/></svg>
+							Minhas Tarefas
+						</button>
+					</li>
+					<li>
+						<button>
+							<svg width="16" height="16" fill="currentColor" class="bi bi-clock-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/></svg>
+							Vencendo
+						</button>
+					</li>
 				</ul>
+				<hr>
 			</div>
 		</aside>
 
