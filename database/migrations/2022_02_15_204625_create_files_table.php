4ffe42d07e1cce274ce6dcc728acaa1d2d0d54a3
Lucas Scemist <scemist.lucas@gmail.com>
2022-02-15 17:59:03 -0300

    Model, Controller e Migration de File, UserSetting e ProjectSetting criados

---
+++ database/migrations/2022_02_15_204625_create_files_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+			$table->id();
+			$table->foreignId('task_id')
+				->references('id')
+				->on('tasks')
+				->onDelete('cascade');
+			$table->foreignId('user_id')
+				->references('id')
+				->on('users')
+				->onDelete('cascade');
+            $table->timestamps();
+			$table->tinyText('path');
+			$table->tinyText('extension');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+};
